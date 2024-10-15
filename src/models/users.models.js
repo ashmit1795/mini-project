29464e1e4a73fb2f8f6253b5d1e241a1fd591cf7
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-15 17:42:13 +0530

    Add user model with required fields

---
+++ src/models/users.models.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+});
+
+export const User = mongoose.model('User', userSchema);
