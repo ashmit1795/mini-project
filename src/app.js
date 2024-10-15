435970fed17ed88292d3d4d345650edc6df5b6d7
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-15 17:41:39 +0530

    add ejs package for view engine support and setup server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 export const app = express();
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+// Static files
+app.use(express.static('public'));
+
+// View engine
+app.set('view engine', 'ejs');
+
+// Import routes
+
+// Use routes
