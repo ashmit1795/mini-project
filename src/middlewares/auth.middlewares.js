c5487ac1b5d6b41949a8dfb4206534079a031c9b
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-21 00:48:16 +0530

    protect middleware to handle unauthorized access and redirect to login page

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -5,22 +5,27 @@ import AsyncHandler from "../utils/AsyncHandler.js";
 
 // Function to protect routes
 export const protect = AsyncHandler(async (req, res, next) => {
-    let token = req.cookies?.accessToken || req.headers["authorization"]?.replace("Bearer ", "");
+    try {
+        let token = req.cookies?.accessToken || req.headers["authorization"]?.replace("Bearer ", "");
 
-    if(!token){
-        res.status(401);
-        throw new AppError(401, "You are Unauthorized");
+        if(!token){
+            res.status(401);
+            throw new AppError(401, "You are Unauthorized");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken.id).select("-password -refreshToken");
+        if(!user){
+            res.status(401);
+            throw new AppError(401, "Unauthorized");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error.message);
+        res.redirect("/app/users/login");
     }
-
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decodedToken.id).select("-password -refreshToken");
-    if(!user){
-        res.status(401);
-        throw new AppError(401, "Unauthorized");
-    }
-
-    req.user = user;
-    next();
 });
 
 
